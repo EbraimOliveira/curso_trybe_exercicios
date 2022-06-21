0dc2a60bf7c85104cb6b0812d86d2de4e1982993
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-21 20:22:46 -0300

    operadores aritméticos

---
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.1_primeiros_passos.js
@@ -0,0 +1,12 @@
+//operadoresaritméticos//
+
+const a = 5;
+const b = 8;
+
+let soma = a + b;
+let subtração = a - b;
+let mutiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+console.log(soma, subtração, mutiplicacao, divisao, modulo);
