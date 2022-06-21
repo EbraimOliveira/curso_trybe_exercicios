4544cab31278b4ac4afcad8332ea83e59418408f
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-21 20:35:42 -0300

    operadores de comparação

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.1_primeiros_passos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.1_primeiros_passos.js
@@ -1,4 +1,4 @@
-//operadoresaritméticos//
+//operadores aritméticos//
 
 const a = 5;
 const b = 8;
@@ -10,3 +10,33 @@ let divisao = a / b;
 let modulo = a % b;
 
 console.log(soma, subtração, mutiplicacao, divisao, modulo);
+
+//Operadores de comparação//
+//Faça um programa que retorne o maior de três números//
+
+const a = 5;
+const b = 8;
+const c = 3;
+
+if (a > b && a > c) {
+  console.log(a);
+}
+if (c > b && c > a) {
+  console.log(c);
+}
+if (a < b && c < b) {
+  console.log(b);
+}
+
+//Faça um programa que, dado um valor definido numa constante, retorne "positive" se esse valor for positivo, "negativo" se for negativo e "zero" caso contrário//
+
+const a = 0;
+
+if (a < 0) {
+  console.log("positivo");
+}
+if (a === 0) {
+  console.log("zero");
+} else {
+  console.log("negativo");
+}
