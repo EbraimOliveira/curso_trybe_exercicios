ebafe9f936af7ecd8105e868505da3a4e3d5e7e5
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-21 21:41:53 -0300

    retorna true se for par

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.1_primeiros_passos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.1_primeiros_passos.js
@@ -94,3 +94,15 @@ if (notaEmPorcentagem < 0 || notaEmPorcentagem > 100) {
 } else {
   console.log("F");
 }
+
+//🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false//
+
+const a = 27;
+const b = 1;
+const c = 33;
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+  console.log(true);
+} else {
+  console.log(false);
+}
