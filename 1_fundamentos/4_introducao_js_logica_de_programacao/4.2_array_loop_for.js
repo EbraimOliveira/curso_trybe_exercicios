22a677bf833a8b9b5e625b526f633aa42ac7da21
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-22 19:29:44 -0300

    exercicio 7 resolvido

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
@@ -65,3 +65,15 @@ if (valoresImpares > 0) {
 } else {
   console.log("nenhum valor impar encontrado");
 }
+
+//7 -Utilizando for, descubra qual o menor valor contido no array e imprima-o.
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[0];
+
+for (let index = 0; index <= numbers.length; index += 1) {
+  if (menorNumero > numbers[index]) {
+    menorNumero = numbers[index];
+  }
+}
+console.log(menorNumero);
