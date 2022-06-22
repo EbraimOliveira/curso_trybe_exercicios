c20f205e88d8180318191b76f4df5d67dedd0025
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-22 19:36:04 -0300

    exercicio 8 resolvido

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
@@ -77,3 +77,11 @@ for (let index = 0; index <= numbers.length; index += 1) {
   }
 }
 console.log(menorNumero);
+
+//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado.
+
+let array = [];
+for (let index = 1; index <= 25; index += 1) {
+  array.push(index);
+}
+console.log(array);
