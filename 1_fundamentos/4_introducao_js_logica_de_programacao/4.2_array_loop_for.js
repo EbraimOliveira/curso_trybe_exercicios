a6407f562456990086104a50cae6bd32472a5143
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-22 19:55:30 -0300

    exercicio 9 resolvido

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.2_array_loop_for.js
@@ -78,10 +78,19 @@ for (let index = 0; index <= numbers.length; index += 1) {
 }
 console.log(menorNumero);
 
-//Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado.
+//8 -Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado.
 
 let array = [];
 for (let index = 1; index <= 25; index += 1) {
   array.push(index);
 }
 console.log(array);
+
+//9 -Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+
+let arrayDividido = [];
+
+for (let index = 0; index < array.length; index += 1) {
+  arrayDividido.push(array[index] / 2);
+}
+console.log(arrayDividido);
