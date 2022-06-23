2670d6e0b6faa1e147a2bb34852832ca8f353ee6
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-23 18:31:05 -0300

    exercicio 03 corrigido o /retorna o menor/

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
@@ -1,31 +1,35 @@
 //1 -Fatorial.
 
-const valor = 10;
-let valorFatorado = valor;
+// const valor = 10;
+// let valorFatorado = valor;
 
-for (let index = valor - 1; index >= 2; index -= 1) {
-  valorFatorado = valorFatorado * index;
-}
-console.log(valorFatorado);
+// for (let index = valor - 1; index >= 2; index -= 1) {
+//   valorFatorado = valorFatorado * index;
+// }
+// console.log(valorFatorado);
 
 //2 -Inverte palavra.
 
-let palavra = "Kakaroto";
-let palavraInvertida = "";
+// let palavra = "Kakaroto";
+// let palavraInvertida = "";
 
-for (let i = palavra.length - 1; i >= 0; i -= 1) {
-  palavraInvertida += palavra[i];
-}
-console.log(palavraInvertida);
+// for (let i = palavra.length - 1; i >= 0; i -= 1) {
+//   palavraInvertida += palavra[i];
+// }
+// console.log(palavraInvertida);
 
 //3 -Retora a maior e a menor.
 
 let array = ["java", "javascript", "python", "html", "css"];
 let maior = array[0];
+let menor = array[0];
 
 for (let i = 1; i < array.length; i += 1) {
   if (maior.length < array[i].length) {
     maior = array[i];
   }
+  if (menor.length > array[i].length) {
+    menor = array[i];
+  }
 }
-console.log(maior);
+console.log(maior, menor);
