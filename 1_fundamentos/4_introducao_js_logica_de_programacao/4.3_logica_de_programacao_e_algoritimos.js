be7c509233c1a587642bd0896612d2ad98e5a175
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-23 15:58:26 -0300

    exercicio 02 resolvido

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
@@ -1,11 +1,19 @@
 //1 -Fatorial.
 
-const valor = 7;
+const valor = 10;
 let valorFatorado = valor;
 
-for (index = valor - 1; index >= 2; index -= 1) {
+for (let index = valor - 1; index >= 2; index -= 1) {
   valorFatorado = valorFatorado * index;
 }
 console.log(valorFatorado);
 
 //2 -Inverte palavra.
+
+let palavra = "Kakaroto";
+let palavraInvertida = "";
+
+for (let i = palavra.length - 1; i >= 0; i -= 1) {
+  palavraInvertida += palavra[i];
+}
+console.log(palavraInvertida);
