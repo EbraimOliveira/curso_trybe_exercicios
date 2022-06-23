7441071dbb7e5d63a47fc32cbe40477817abfaa4
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-23 19:30:49 -0300

    exercicio 04 concluido

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
@@ -20,16 +20,33 @@
 
 //3 -Retora a maior e a menor.
 
-let array = ["java", "javascript", "python", "html", "css"];
-let maior = array[0];
-let menor = array[0];
+// let array = ["java", "javascript", "python", "html", "css"];
+// let maior = array[0];
+// let menor = array[0];
 
-for (let i = 1; i < array.length; i += 1) {
-  if (maior.length < array[i].length) {
-    maior = array[i];
+// for (let i = 1; i < array.length; i += 1) {
+//   if (maior.length < array[i].length) {
+//     maior = array[i];
+//   }
+//   if (menor.length > array[i].length) {
+//     menor = array[i];
+//   }
+// }
+// console.log(maior, menor);
+
+//4 -Números primos.
+
+let maiorNumeroPrimo = 2;
+
+for (let i = 3; i <= 22; i += 1) {
+  let contadorNaoPrimo = 0;
+  for (let i2 = 2; i2 < i; i2 += 1) {
+    if (i % i2 === 0) {
+      contadorNaoPrimo += 1;
+    }
   }
-  if (menor.length > array[i].length) {
-    menor = array[i];
+  if (contadorNaoPrimo === 0) {
+    maiorNumeroPrimo = i;
   }
 }
-console.log(maior, menor);
+console.log(maiorNumeroPrimo);
