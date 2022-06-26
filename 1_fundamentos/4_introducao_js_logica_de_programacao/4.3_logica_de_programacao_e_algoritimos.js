0cac5888b60324a1484bf245cb72637de7c571ff
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2022-06-26 19:55:56 -0300

    inicio da resolucao do exercicio bonus de esteriscos

--- 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
+++ 1_fundamentos/4_introducao_js_logica_de_programacao/4.3_logica_de_programacao_e_algoritimos.js
@@ -1,52 +1,67 @@
-//1 -Fatorial.
+// //1 -Fatorial.
 
-// const valor = 10;
-// let valorFatorado = valor;
+// // const valor = 10;
+// // let valorFatorado = valor;
 
-// for (let index = valor - 1; index >= 2; index -= 1) {
-//   valorFatorado = valorFatorado * index;
-// }
-// console.log(valorFatorado);
+// // for (let index = valor - 1; index >= 2; index -= 1) {
+// //   valorFatorado = valorFatorado * index;
+// // }
+// // console.log(valorFatorado);
 
-//2 -Inverte palavra.
+// //2 -Inverte palavra.
 
-// let palavra = "Kakaroto";
-// let palavraInvertida = "";
+// // let palavra = "Kakaroto";
+// // let palavraInvertida = "";
 
-// for (let i = palavra.length - 1; i >= 0; i -= 1) {
-//   palavraInvertida += palavra[i];
-// }
-// console.log(palavraInvertida);
+// // for (let i = palavra.length - 1; i >= 0; i -= 1) {
+// //   palavraInvertida += palavra[i];
+// // }
+// // console.log(palavraInvertida);
 
-//3 -Retora a maior e a menor.
+// //3 -Retora a maior e a menor.
 
-// let array = ["java", "javascript", "python", "html", "css"];
-// let maior = array[0];
-// let menor = array[0];
+// // let array = ["java", "javascript", "python", "html", "css"];
+// // let maior = array[0];
+// // let menor = array[0];
 
-// for (let i = 1; i < array.length; i += 1) {
-//   if (maior.length < array[i].length) {
-//     maior = array[i];
+// // for (let i = 1; i < array.length; i += 1) {
+// //   if (maior.length < array[i].length) {
+// //     maior = array[i];
+// //   }
+// //   if (menor.length > array[i].length) {
+// //     menor = array[i];
+// //   }
+// // }
+// // console.log(maior, menor);
+
+// //4 -Números primos.
+
+// let maiorNumeroPrimo = 2;
+
+// for (let i = 3; i <= 22; i += 1) {
+//   let contadorNaoPrimo = 0;
+//   for (let i2 = 2; i2 < i; i2 += 1) {
+//     if (i % i2 === 0) {
+//       contadorNaoPrimo += 1;
+//     }
 //   }
-//   if (menor.length > array[i].length) {
-//     menor = array[i];
+//   if (contadorNaoPrimo === 0) {
+//     maiorNumeroPrimo = i;
 //   }
 // }
-// console.log(maior, menor);
+// console.log(maiorNumeroPrimo);
 
-//4 -Números primos.
+//1 Bônus -Quadrado de asteriscos.
 
-let maiorNumeroPrimo = 2;
+let n = 3;
 
-for (let i = 3; i <= 22; i += 1) {
-  let contadorNaoPrimo = 0;
-  for (let i2 = 2; i2 < i; i2 += 1) {
-    if (i % i2 === 0) {
-      contadorNaoPrimo += 1;
+function asteriscos(n) {
+  for (let index = 1; index <= n; index += 1) {
+    let quadradoDeAsteriscos = " ";
+
+    for (let index2 = 1; index2 <= n; index2 += 1) {
+      quadradoDeAsteriscos = +"*";
     }
-  }
-  if (contadorNaoPrimo === 0) {
-    maiorNumeroPrimo = i;
+    console.log(quadradoDeAsteriscos);
   }
 }
-console.log(maiorNumeroPrimo);
